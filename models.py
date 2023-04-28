9b347f6a8a56ecbaedddd232f0a0e485a0a08f33
Michael <mherman315@gmail.com>
2023-04-28 12:18:29 -0700

    Pet model made, working on routes + html

--- models.py
+++ models.py
@@ -14,3 +14,45 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    '''pets.'''
+
+    __tablename__ = 'pets'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    species = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=''
+    )
+
+    age = db.Column(
+        db.String(6),
+        nullable=False
+    )
+
+    notes = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
